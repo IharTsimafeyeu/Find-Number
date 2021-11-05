6d0f9025441cc2f45fdc3cba5f468c7d5b6aaebd
Ihar Tsimafeyeu <ColdSun1395@gmail.com>
2021-11-05 12:59:27 +0300

    commit

---
+++ Find Number/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Find Number
+//
+//  Created by Игорь Тимофеев on 5.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
